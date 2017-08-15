9781c665764be188d7fa8294e02428c3b208de05
jpkhalsa007 <30776160+jpkhalsa007@users.noreply.github.com>
2017-08-15 13:39:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function(req, res){
+    res.send('Article one required');
+} );
+app.get('/article-two', function(req, res){
+    res.send('Article two required');
+} );
+
+app.get('/article-three', function(req, res){
+    res.send('Article three required');
+} );
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
